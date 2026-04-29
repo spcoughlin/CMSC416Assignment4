aaf67fc56b5b3be111baa16fa0a3ec9b8c2e996b
Sean Coughlin <56spc56@gmail.com>
2026-04-29 11:03:28 -0400

    python reference

--- a4-code/kmeans_omp.c
+++ a4-code/kmeans_omp.c
@@ -1,2 +1,36 @@
 // kmeans_omp.c for problem 1
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+
+int filestats(char *filename, ssize_t *tot_tokens, ssize_t *tot_lines);
+
+typedef struct {
+    int ndata;
+    int dim;
+    float *features;
+    int *assigns;
+    int *labels;
+    int nlabels;
+} KMData;
+
+typedef struct {
+    int nclust;
+    int dim;
+    float *features;
+    int *counts;
+} KMClust;
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("usage: kmeans_serial <datafile> <nclust> [savedir] [maxiter]\n");
+        return 1;
+    }
+
+    const char *datafile = argv[1];
+    int nclust = atoi(argv[2]);
+    const char *savedir = ".";
+    int MAXITER = 100;
